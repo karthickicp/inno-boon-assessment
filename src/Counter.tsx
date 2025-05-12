0276a1650096b7def0d0cc343b327f06d7d106a5
iamkarthi <karthickicp15@gmail.com>
2025-05-12 16:51:51 +0530

    Fixed edge cases

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -3,13 +3,15 @@ import { Box, Button, Card, Grid } from "@mui/material";
 import { CounterContext } from "./context";
 export const Counter = () => {
   const counter = useContext(CounterContext);
-  console.log(counter, "counter");
   return (
     <div className="body-view">
-      {counter.count ? (
+      {counter.count > 0 ? (
         <Grid container spacing={2}>
-          {[...new Array(counter.count)].map((_, index) => (
-            <Grid size={{ xs: 12, sm: 6, md: 4, lg: 3 }}>
+          {[...new Array(counter?.count)].map((_, index) => (
+            <Grid
+              size={{ xs: 12, sm: 6, md: 4, lg: 3 }}
+              key={`card-${index + 1}`}
+            >
               <Card sx={{ padding: "1rem" }}>
                 <h1>{index + 1}</h1>
               </Card>
@@ -21,7 +23,12 @@ export const Counter = () => {
         <Button variant="contained" color="primary" onClick={counter.increment}>
           Increment
         </Button>
-        <Button variant="outlined" color="success" onClick={counter.decrement}>
+        <Button
+          variant="outlined"
+          color="success"
+          onClick={counter.decrement}
+          disabled={counter.count < 1}
+        >
           Decrement
         </Button>
       </Box>
